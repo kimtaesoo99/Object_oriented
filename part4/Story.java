45aff9a8c31e21989c830e99cefcf773c93ed8eb
kimtaesoo <kimtaesoo7@naver.com>
2023-04-26 21:47:30 +0900

    refactor: 메서드 분리

--- part4/Story.java
+++ part4/Story.java
@@ -8,37 +8,41 @@ import part4.witness.Witness;
 import java.util.Scanner;
 
 public class Story {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("판사를 선택하시오. [왕, 왕비] 중에 골라야한다.");
-
-        Judge judge;
-        while (true) {
-            String input = sc.next();
-            try {
-                judge = JudgeType.getJudge(input);
-                break;
-            } catch (IllegalStateException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
+        Judge judge = getJudge();
         judge.start();
-        Witness witness;
         Rabbit rabbit = new Rabbit();
+        testify(judge, rabbit);
+        judge.end();
+    }
+
+    private static Judge getJudge() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.next();
+            return JudgeType.getJudge(input);
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+            return getJudge();
+        }
+    }
+
+    private static void testify(Judge judge, Rabbit rabbit) {
         while (true) {
             System.out.println("증인을 선택하시오. [모자장수, 엘리스, 요리사] 없을시 종료를 입력하시오.");
             String input = sc.next();
             if (input.equals("종료")) {
-                break;
+                return;
             }
             try {
-                witness = judge.callByWitness(rabbit, input);
+                Witness witness = judge.callByWitness(rabbit, input);
                 judge.askForTestimony(witness);
             } catch (IllegalStateException e) {
                 System.out.println(e.getMessage());
             }
         }
-        judge.end();
     }
 }
